780b42760adeed229326a861c305b6cd46a4d410
hmalin <lloydmalin15@gmail.com>
2022-12-14 22:44:21 -0800

    first java project -Hello World

--- hello.java
+++ hello.java
@@ -1,13 +1,60 @@
-/**
- * hello
- */
-public class hello {
+package com.lmalin.hello;
 
-    /**
-     * @param args
-     */
-    public static void main(String[]args){
-        System.out.println("Hello World!");
+/*
+          COMPILING JAVA:
+In Java, when we compile a program, each individual class is converted into a .
+class file, which is known as byte code.
+The JVM (Java virtual machine) is used to run the byte code.
 
+*/
+public class Hello {
+      //////CLASS:
     
-    }}
+ /*
+    A class represents a single concept.
+
+A Java program must have one class whose name is the same as the program filename.
+
+In the example, the Person class must be declared in a program file named Person.java.
+
+ */
+
+    public static void main(String[] args) 
+   ///MAIN:
+    /*
+    In Java, every application must contain a main() method, 
+    which is the entry point for the application. 
+    All other methods are invoked from the main() method.
+    */
+    
+    
+    
+    {
+        System.out.println("Hello World!");
+     /*
+     STATEMENTS:
+In Java, a statement is a line of code that executes a task and is terminated with a ;
+system is a class from the core java library
+out is an object that contols output
+println() is a method associated with the object.
+Outputs: Hello World! welcome to my first project!
+  */
+
+  /*
+     WHITESPACE:
+     Whitespace, including spaces and newlines, between statements is ignored.
+     */
+
+  ///COMMENTS:
+
+// single line coment 
+
+/*
+Multi
+line 
+comment
+*/
+     }
+
+
+}
