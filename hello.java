f11bd7feda791afa22c020979f26a0baa6b6b4ea
hmalin <lloydmalin15@gmail.com>
2022-12-14 22:41:32 -0800

    first java project -Hello World

---
+++ hello.java
@@ -0,0 +1,13 @@
+/**
+ * hello
+ */
+public class hello {
+
+    /**
+     * @param args
+     */
+    public static void main(String[]args){
+        System.out.println("Hello World!");
+
+    
+    }}
